0d0d21e82f230d7571f086d12fab40e68836d555
moemyself3 <moemyself3@gmail.com>
2024-07-18 11:51:23 -0500

    Add Helper class (show_files), tests for make_darks, make_flats

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,42 +7,69 @@ from astropy.nddata import CCDData
 from astropy.io import fits
 import ccdproc
 
+MAINDIR = os.getcwd()
+
+class Helper:
+    def show_files(path):
+        print(f"Scanning Directory: {path}")
+        with os.scandir(path) as iterable_items:
+            print("Name\t\tType")
+            for entry in iterable_items:
+                if entry.is_file():
+                    entry_type = "file"
+                    print(f"{entry.name}\t{entry_type}")
+                
+                if entry.is_dir():
+                    entry_type = "dir"
+                    print(f"{entry.name}\t\t{entry_type}")
+                    with os.scandir(path / entry.name ) as iterable_dir:
+                        for subfile in iterable_dir:
+                            print(f" |-> {subfile.name}")
+    
 @pytest.fixture(scope="session")
-def fit_files(tmp_path_factory):
+def helper():
+    return Helper
+
+@pytest.fixture(scope="session")
+def obj_dir(tmp_path_factory):
+    fit_files_path = tmp_path_factory.mktemp("data")
     # Make directories for fit files
     #  needs data, wcs, and align
-    fit_files_path = tmp_path_factory.mktemp("data")
     wcs_path = fit_files_path / "wcs"
     wcs_path.mkdir()
     align_path = fit_files_path / "align"
     align_path.mkdir()
-    
+    dark_path = fit_files_path / "dark"
+    dark_path.mkdir()
+
     # Make fit file data
     hdu = example_hdu()
     print(f"Making file path: {fit_files_path}")
     _ = [hdu.writeto(fit_files_path / f"ccd-{i}.fit") for i in range(3)]
-
     _ = [hdu.writeto(wcs_path / f"wcs-{i}.fit") for i in range(3)]
+    _ = [hdu.writeto(dark_path/ f"dark-{i}.fit") for i in range(3)]
 
-    print(f"Scanning Directory: {fit_files_path}")
-    with os.scandir(fit_files_path) as iterable_items:
-        print("Name\tType")
-        for entry in iterable_items:
-            if entry.is_dir():
-                entry_type = "dir"
-            elif entry.is_file():
-                entry_type = "file"
-            else:
-                entry_type = None
-            print(f"{entry.name}\t{entry_type}")
-    
+    Helper.show_files(fit_files_path)
+
     return fit_files_path
+
+@pytest.fixture(scope="session")
+def flat_dir(obj_dir):
+    return obj_dir
+
+@pytest.fixture(scope="session")
+def dark_dir(obj_dir):
+    return obj_dir
+
+@pytest.fixture(scope="session")
+def object_frame():
+    return example_hdu()
 
 def example_ccd_data():
     return CCDData(np.random.rand(2,2), unit="adu")
 
 def example_header_with_wcs():
-    path = Path(os.getcwd())
+    path = Path(MAINDIR)
     path = path / 'tests'/ 'example_header.txt'
     with open(path, 'r') as file:
         data = file.read()
@@ -53,3 +80,20 @@ def example_header_with_wcs():
 def example_hdu():
     hdu = fits.PrimaryHDU(example_ccd_data(), example_header_with_wcs())
     return hdu
+
+def show_files(path):
+    print(f"Scanning Directory: {path}")
+    with os.scandir(path) as iterable_items:
+        print("Name\t\tType")
+        for entry in iterable_items:
+            if entry.is_file():
+                entry_type = "file"
+                print(f"{entry.name}\t{entry_type}")
+            
+            if entry.is_dir():
+                entry_type = "dir"
+                print(f"{entry.name}\t\t{entry_type}")
+                with os.scandir(path / entry.name ) as iterable_dir:
+                    for subfile in iterable_dir:
+                        print(f" |-> {subfile.name}")
+
--- tests/test_reducer.py
+++ tests/test_reducer.py
@@ -1,17 +1,38 @@
 import pytest
 import os
+import astropy
 
 from pathlib import Path
 
 from libraries.reducer import Reducer
 
 def test_align_frames_bad_path():
-    obj_dir = 'qweruiopasdfjkl;'
+    bad_path = 'qweruiopasdfjkl;'
     with pytest.raises(FileNotFoundError):
-        Reducer.align_frames(obj_dir)
+        Reducer.align_frames(bad_path)
 
-def test_align_frames(fit_files):
-   assert isinstance(Reducer.align_frames(fit_files), list)
-   aligned_frames = os.listdir(fit_files / 'align')
+def test_align_frames(obj_dir):
+   assert isinstance(Reducer.align_frames(obj_dir), list)
+   aligned_frames = os.listdir(obj_dir / 'align')
    print(f"aligned frames dir: {aligned_frames}")
-   assert len(os.listdir(fit_files / 'align')) > 0
+   assert len(os.listdir(obj_dir / 'align')) > 0
+
+def test_make_dark(dark_dir, helper):
+    assert isinstance(Reducer.make_dark(dark_dir), astropy.nddata.CCDData)
+    helper.show_files(dark_dir)
+
+def test_make_flat(flat_dir, dark_dir, helper):
+    assert isinstance(Reducer.make_flat(flat_dir, dark_dir), astropy.nddata.CCDData)
+    helper.show_files(flat_dir)
+
+def test_make_mask(object_frame):
+    pass
+
+def test_make_stack(obj_dir):
+    pass
+
+def test_reduce_objects(obj_dir, flat_dir, dark_dir, bkg_method='flat'):
+    pass
+
+def test_solve_plates(obj_dir):
+    pass
